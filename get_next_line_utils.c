9e822e0ed1a5db355fec4a836730a52d8db4cdf2
Hyunwoo Kim <hyunwkim@c10r4s12.42seoul.kr>
2021-06-26 16:23:14 +0900

    feat: fix strjoin

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: hyunwkim <hyunwkim@42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/22 10:09:07 by hyunwkim          #+#    #+#             */
-/*   Updated: 2021/06/23 18:05:48 by hyunwkim         ###   ########.fr       */
+/*   Updated: 2021/06/26 16:18:47 by hyunwkim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -73,7 +73,7 @@ char			*ft_strjoin(char *s1, char *s2, int read_size)
 		return (ft_strdup(s2));
 	if (!s2)
 		return (ft_strdup(s1));
-	s1[read_size] = 0;
+	s2[read_size] = 0;
 	len_s1 = ft_strlen(s1);
 	len_s2 = ft_strlen(s2);
 	idx = 0;
