4fe1c9093f447c05a85de8ac6d7e2dcd0f2d30b7
Hyunwoo Kim <hyunwkim@c10r4s6.42seoul.kr>
2021-06-28 12:16:52 +0900

    feat: done

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -6,7 +6,7 @@
 /*   By: hyunwkim <hyunwkim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/18 13:35:13 by hyunwkim          #+#    #+#             */
-/*   Updated: 2021/06/28 12:10:26 by hyunwkim         ###   ########.fr       */
+/*   Updated: 2021/06/28 12:15:52 by hyunwkim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,9 +63,9 @@ int				get_next_line(int fd, char **line)
 	char		*buff;
 	static char *bak[256];
 
-	*line = 0;
 	if (fd < 0 || BUFFER_SIZE <= 0 || line == 0 || fd > 255)
 		return (-1);
+	*line = 0;
 	if (!(buff = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1))))
 		return (-1);
 	while ((newline_idx = is_newline(bak[fd])) == -1)
